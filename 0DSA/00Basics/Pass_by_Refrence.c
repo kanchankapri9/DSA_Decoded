a743a603f178c22cecbdbf1c21265f1624de0abd
kanchan kapri <kaprikanchan09@gmail.com>
2026-07-08 21:35:10 +0530

    New commit message

---
+++ 0DSA/00Basics/Pass_by_Refrence.c
@@ -0,0 +1,74 @@
+                               // Pass by Value vs Pass by Refrence
+//------------------------------------------------------------------------------------------------
+
+
+// Pass by Value 
+
+/*
+
+Pass by value  is
+
+
+
+*/
+
+
+
+#include <stdio.h>
+using namespace std;
+
+
+                  
+void hi() {
+
+    cout << i << endl;
+    i += 7;
+    cout << i << endl;
+    
+    i += 4;
+    cout << i << endl;
+
+
+
+}
+
+
+int main () {
+    int i = 1;
+    cout << i << endl;
+    hi();
+    cout << i << endl;
+    cout << i << endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------
+
+// Pass by refrence
+
+/*
+
+Pass by refrence
+
+
+
+*/
+
+
+
