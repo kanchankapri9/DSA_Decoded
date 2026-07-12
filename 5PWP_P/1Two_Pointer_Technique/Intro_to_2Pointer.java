325469603cc820a209575daa559b5ed3ceea7693
kanchan kapri <kaprikanchan09@gmail.com>
2026-07-12 23:05:29 +0530

    D1

---
+++ 5PWP_P/1Two_Pointer_Technique/Intro_to_2Pointer.java
@@ -0,0 +1,62 @@
+// The 2 Pointer Technique is a very useful technique to solve problems on arrays and strings.
+//  It is used when we have to find pairs in an array or string that satisfy a certain condition. 
+// The idea is to use two pointers to traverse the array or string from both ends towards the center,
+//  which can help reduce the time complexity of the solution.
+
+
+// the 2 pointer techniques is simply a way to solve a problem by using two pointers to traverse the array or sting in half time by adding two pointers 
+// ( as one pointer from the start and one pointere from the end ) and moving them towards each othere until they meet or cross each other. 
+// the benafits we have by using these techniques are that we can reduce the time complexity of the solution from O(n^2) to O(n) in many cases.
+
+
+
+// usecases in sanario of :
+// 1. when we have to find pairs in an array or string that satisfy a certain condition.
+// 2. when we have to find the longest substring or subarray that satisfies a certain condition.
+
+
+// when we can think that yes we can apply the Two Pointers ???
+/*
+
+1. If data is Sorted , or making it sort can be benaficial . or Merge , remove Duplicates , remove duplicates from sorted array or string , remove duplicates from unsorted array or string .
+2. If we have to find pairs in an array or string that satisfy a certain condition.
+3. If we have to find the longest substring or subarray that satisfies a certain condition.
+
+4. Question of array or Linked List.  = Possibility = 10 percent. out of 10 .
+
+5. not queue not stack not tree not graph.  = Possibility = 5 percent.
+
+6. Rearrange, Dedect cycle in Linked Liist , of you think that if there was any cycle that answer could be found.
+7. find in pair , triplets , Quardriplets , subarray , substring , longest substring , longest subarray , shortest substring , shortest subarray.
+
+
+
+quadruples : which has more then one answer.
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+                   /*      
+
+Summury :
+
+1. Two Pointer Technique is a very useful technique to solve problems on arrays and strings.
+2. It is used when we have to find pairs in an array or string that satisfy a certain condition. 
+3. The idea is to use two pointers to traverse the array or string from both ends towards the center,
+   which can help reduce the time complexity of the solution.
+
+                    */
+
+
+   
+
+
